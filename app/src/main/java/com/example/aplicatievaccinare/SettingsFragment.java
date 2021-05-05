1b5e249994d2897e582f0e3d109ef73dd7ffadb9
andreiec <andreiec112@gmail.com>
2021-05-05 14:37:27 +0300

    Finished structure of profile page, moved setting button in the navigation menu, started working on map page

--- app/src/main/java/com/example/aplicatievaccinare/SettingsFragment.java
+++ app/src/main/java/com/example/aplicatievaccinare/SettingsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProgramFragment#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProgramFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class ProgramFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ProgramFragment() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class ProgramFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProgramFragment.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProgramFragment newInstance(String param1, String param2) {
-        ProgramFragment fragment = new ProgramFragment();
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class ProgramFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_program, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
