4616870090749c9c3e65ead2cf583359f193fe79
andreiec <andreiec112@gmail.com>
2021-05-07 23:05:36 +0300

    Changed map pin icon and implemented custom map themes part 2

--- app/src/main/java/com/example/aplicatievaccinare/MapsFragment.java
+++ app/src/main/java/com/example/aplicatievaccinare/MapsFragment.java
@@ -1,10 +1,18 @@
 package com.example.aplicatievaccinare;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +21,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap mMap;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -31,11 +46,18 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap = googleMap;
+
+            LatLng sydney = new LatLng(44.37733037098994, 26.15013279240117);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Popesti-Leordeni - Motanu")).setIcon(bitmapDescriptorFromVector(requireContext(), R.drawable.ic_baseline_cross));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12F));
+
+            //TODO REZOLVA
+            mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.map_style));
+
         }
     };
+
 
     @Nullable
     @Override
@@ -53,5 +75,28 @@ public class MapsFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d("RecyclerViewAdapter", "am apasat pe ma-ta" + marker.getTitle());
+        return false;
+    }
+
+    // Implement Vector to Bitmap for pin
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable background = ContextCompat.getDrawable(context, R.drawable.ic_baseline_pin);
+        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(14, 14, vectorDrawable.getIntrinsicWidth() + 14, vectorDrawable.getIntrinsicHeight() + 14);
+
+        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        background.draw(canvas);
+        vectorDrawable.draw(canvas);
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
